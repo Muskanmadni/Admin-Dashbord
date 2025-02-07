cae22c2b831b60b2b3cfef702f954743d3a6b08d
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-02-07 10:54:48 +0500

    first commit

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -44,6 +44,7 @@ export default function AdminDashboard() {
             Swal.fire("Success", `Order status updated to ${newStatus}`, "success");
         } catch (error) {
             Swal.fire( "Failed to update order");
+            console.error(error);
         }
     }
 
@@ -66,6 +67,7 @@ export default function AdminDashboard() {
             Swal.fire('Deleted!', 'Order successfully deleted', "success");
         } catch (error) {
             Swal.fire("Error", "Failed to delete order", "error");
+            console.error(error);
         }
     }
 
